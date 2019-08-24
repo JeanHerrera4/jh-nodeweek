6a6e9d9da1f55c0b4b7510f48b1e378a7688f303
Jeanheme <Jherreram@sura.com.co>
2019-08-24 11:31:09 -0500

    Second Commit

--- handler.js
+++ handler.js
@@ -1,16 +1,20 @@
 'use strict';
 
-module.exports.hello = async event => {
+module.exports.otronombre = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
+    headers: {
+      "Content-Type": "text/html"
+    },
+    body: '<html><body>hola nodeweek</body></html>'
+    /*body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Hola nodeweek!',
         input: event,
       },
       null,
       2
-    ),
+    ),*/
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
